e9b98aa040b533894f32d2cbe0e338e1ebe2eeaa
Matheus Leite <matheus.leite.san@gmail.com>
2020-04-03 15:11:35 -0300

    Add a Key verification

--- src/bot/bot_actions.py
+++ src/bot/bot_actions.py
@@ -12,9 +12,7 @@ class BotActions:
         )
 
     def retrieve_non_command_phrase(self, update, context):
-        # @todo remove /start and add and specific command validation
-        if '/start' not in context.args[0]:
-            return context.bot.send_message(
-                chat_id=update.effective_chat.id,
-                text=NonCommandPhrase().retrieve_random_phrase()
-            )
+        return context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text=NonCommandPhrase().retrieve_random_phrase()
+        )
